54486b6ff24a2664d37e06891b7595501d6581a0
hoopox-web <web@hoopox.com>
2019-12-10 18:13:49 +0800

    chore: add google verification tag

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ var content = require('./index.ejs');
 module.exports = layout(content, {
     title: '北京好扑科技【官网】',
     keywords: '好扑,好扑科技,hoopox,北京好扑,北京好扑科技公司',
-    description: '北京好扑信息科技有限公司(Hoopox)致力于推出去中心化云计算区块链网络，降低用户使用和管理成本，提高区块链开发、创建、部署的工作效率，提升数据存储方面的安全性。'
+    description: '北京好扑信息科技有限公司(Hoopox)致力于推出去中心化云计算区块链网络，降低用户使用和管理成本，提高区块链开发、创建、部署的工作效率，提升数据存储方面的安全性。',
+    googleVerification: '<meta name="google-site-verification" content="idBYSUJ8vpv7DYcz9-rwkrU3DO4cD0qwhlK0XHqhU5M" />'
 });
