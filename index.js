db8e67bb3912165e2cf0e87257c9f52e6ed4643c
hoopox-web <web@hoopox.com>
2019-12-13 11:22:59 +0800

    chore: add baidu verification

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ module.exports = layout(content, {
     title: '北京好扑科技【官网】',
     keywords: '好扑,好扑科技,hoopox,北京好扑,北京好扑科技公司',
     description: '北京好扑信息科技有限公司(Hoopox)致力于推出去中心化云计算区块链网络，降低用户使用和管理成本，提高区块链开发、创建、部署的工作效率，提升数据存储方面的安全性。',
-    googleVerification: '<meta name="google-site-verification" content="4Af7ealqlaWiEVGGrucjyLTAnts7PXlhnuqNgcTj7j0" />'
+    googleVerification: '<meta name="google-site-verification" content="4Af7ealqlaWiEVGGrucjyLTAnts7PXlhnuqNgcTj7j0" />',
+    baiduVerification: '<meta name="baidu-site-verification" content="B0jMjEtCuX" />'
 });
