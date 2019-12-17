3e2e040029df2e25f398c265b93c4079a858ff5f
hoopox-web <web@hoopox.com>
2019-12-17 12:03:38 +0800

    chore: add baidu verify

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = layout(content, {
     title: '北京好扑科技【官网】',
     keywords: '好扑,好扑科技,hoopox,北京好扑,北京好扑科技公司',
     description: '北京好扑信息科技有限公司(Hoopox)致力于推出去中心化云计算区块链网络，降低用户使用和管理成本，提高区块链开发、创建、部署的工作效率，提升数据存储方面的安全性。',
-    googleVerification: '<meta name="google-site-verification" content="4Af7ealqlaWiEVGGrucjyLTAnts7PXlhnuqNgcTj7j0" />',
-    baiduVerification: '<meta name="baidu-site-verification" content="uceaRdovvK" />',
+    googleVerification: '<meta name="google-site-verification" content="idBYSUJ8vpv7DYcz9-rwkrU3DO4cD0qwhlK0XHqhU5M" />',
+    baiduVerification: '<meta name="baidu-site-verification" content="uceaRdovvK" />\n<meta name="baidu-site-verification" content="ZYdh9F9cmq" />',
     bingVerification: '<meta name="msvalidate.01" content="1E41448A23B147A2772183A7549469A1" />'
 });
--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -6,6 +6,9 @@ const moment = require('moment');
 const now = moment().utcOffset(0).format('YYYY-MM-DDTHH:mm:ssZ');
 
 const siteMap = fs.readFileSync(path.resolve('./scripts/sitemap.xml')).toString();
+const wwwSiteMap = fs.readFileSync(path.resolve('./scripts/www-sitemap.xml')).toString();
 const result = siteMap.replace(/%lastModified%/g, now);
+const wwwResult = wwwSiteMap.replace(/%lastModified%/g, now);
 
 fs.writeFileSync(path.resolve('./dist/sitemap.xml'), result);
+fs.writeFileSync(path.resolve('./dist/www-sitemap.xml'), wwwResult);
